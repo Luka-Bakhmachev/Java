ad69378b8dce18d33a72e30d6ee0ba55700ef5dd
Luka <lb605112714@gmail.com>
2025-11-06 19:12:01 +0100

    hello blyat

---
+++ src/AEA3/test.java
@@ -0,0 +1,10 @@
+package AEA3;
+
+import java.util.Scanner;
+
+public class test{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Hola como te llamas");
+    }
+}

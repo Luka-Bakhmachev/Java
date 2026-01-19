dd0a42ede0f1ba79ca0c690f7b93420209fae2b1
Funk13 <lb605112714@gmail.com>
2026-01-19 20:43:05 +0100

    aea3 Calcularees final

--- src/AritmeticaModulos/Aritmetica.java
+++ src/AritmeticaModulos/Aritmetica.java
@@ -1,10 +1,46 @@
 package AritmeticaModulos;
 
+import java.util.Scanner;
+
 public class Aritmetica {
-    public static void triangle(){
+    //private static double area = 0;
+
+    public static double triangle(){
         double b = 23.0;
         double h = 8.0;
-        area = (b*h)/2;
+        return (b*h)/2;
+    }
+    public static double quadrat(){
+        double r = 37.0;
+        return Math.pow(r, 2);
+    }
+    public static double rectangle(){
+        double a = 68.0;
+        double b = 39.0;
+        return a*b;
+    }
+    public static double trapezi(){
+        double a = 35.0;
+        double b = 16.0;
+        double h = 5.0;
+        return ((a*b)*h)/2;
+    }
+    public static double rombe(){
+        double dM = 75.0;
+        double dm = 30.0;
+        return (dM*dm)/2;
+    }
+    public static double paralelogram(){
+        double b = 45.0;
+        double h = 13.0;
+        return b*h;
+    }
+    public static double cercle(Scanner s){
+        System.out.println("Quin es el radi del cercle?");
+        double r = s.nextDouble();
+        //funció matemàtica que eleva a 2 el radi
+        //Math.PI = nombre pi (3.14159....)
+        return Math.pow(r, 2)*Math.PI; // area = r * r* 3.14;
     }
 
     
--- src/AritmeticaModulos/CalculArees.java
+++ src/AritmeticaModulos/CalculArees.java
@@ -9,8 +9,8 @@ public class CalculArees {
     programa.inici();
     }
 
-    
-    public  void inici() {
+
+    public void inici() {
     Scanner s = new Scanner(System.in);
 
 
@@ -19,53 +19,17 @@ public class CalculArees {
 
     String figura = s.nextLine();
     switch(figura){
-        case "triangle" -> Aritmetica.triangle();
-        case "quadrat" -> quadrat();
-        case "rectangle" -> rectangle();
-        case "trapezi" -> trapezi();
-        case "rombe" -> rombe();
-        case "paralelogram" -> paralelogram();
-        case "cercle" -> cercle(s);
+        case "triangle" -> area = Aritmetica.triangle();
+        case "quadrat" -> area = Aritmetica.quadrat();
+        case "rectangle" -> area = Aritmetica.rectangle();
+        case "trapezi" -> area = Aritmetica.trapezi();
+        case "rombe" -> area = Aritmetica.rombe();
+        case "paralelogram" -> area = Aritmetica.paralelogram();
+        case "cercle" -> area = Aritmetica.cercle(s);
         default -> {
             System.out.println("Aquesta figura no està contemplada al programa.");
         }
     }
     System.out.println("L'area de la figura " + figura + " és: " + area);
-    }
-
-
-  
-    
-    public static void quadrat(){
-        double r = 37.0;
-        area = Math.pow(r, 2);
-    }
-    public static void rectangle(){
-        double a = 68.0;
-        double b = 39.0;
-        area = a*b;
-    }
-    public static void trapezi(){
-        double a = 35.0;
-        double b = 16.0;
-        double h = 5.0;
-        area = ((a*b)*h)/2;
-    }
-    public static void rombe(){
-        double dM = 75.0;
-        double dm = 30.0;
-        area = (dM*dm)/2;
-    }
-    public static void paralelogram(){
-        double b = 45.0;
-        double h = 13.0;
-        area = b*h;
-    }
-    public static void cercle(Scanner s){
-        System.out.println("Quin es el radi del cercle?");
-        double r = s.nextDouble();
-        //funció matemàtica que eleva a 2 el radi
-        //Math.PI = nombre pi (3.14159....)
-        area = Math.pow(r, 2)*Math.PI; // area = r * r* 3.14;
     }
 }

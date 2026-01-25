c4ca8f3fa190075ea6a2885be9b8662ab9a4096b
Funk13 <lb605112714@gmail.com>
2026-01-25 22:48:09 +0100

    Fix

--- src/AEA3/min_max_mitj/Main.java
+++ src/AEA3/min_max_mitj/Main.java
@@ -3,18 +3,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner s = new Scanner(System.in);
         double[] numbers = new double[5];
 
         for (int i = 0; i < numbers.length; i++) {
             System.out.print("Enter number " + (i + 1) + ": ");
-            numbers[i] = sc.nextDouble();
+            numbers[i] = s.nextDouble();
         }
-
+        //Is this unique and cool enough? 
         System.out.println("Highest: " + Calculator.getMax(numbers));
         System.out.println("Lowest: " + Calculator.getMin(numbers));
         System.out.println("Average: " + Calculator.getAverage(numbers));
-
-        sc.close();
     }
 }
